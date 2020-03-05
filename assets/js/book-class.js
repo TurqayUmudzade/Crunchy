6a19ebb7c1c91df2682d865932de392100e9f858
turgayau@code.edu.az <turgayau@code.edu.az>
2020-03-05 23:12:55 +0400

    3 pages done with charts

--- assets/js/book-class.js
+++ assets/js/book-class.js
@@ -10,3 +10,45 @@ $(".js-book-class").on("click", function() {
 
     $(this).parent().next().removeClass("hidden");
 });
+
+$("#d1").on("click", function() {
+    $('article').addClass("hidden");
+    $("#a1").removeClass("hidden");
+
+});
+
+$("#d2").on("click", function() {
+    $('article').addClass("hidden");
+    $("#a2").removeClass("hidden");
+
+});
+
+$("#d3").on("click", function() {
+    $('article').addClass("hidden");
+    $("#a3").removeClass("hidden");
+
+});
+
+$("#d4").on("click", function() {
+    $('article').addClass("hidden");
+    $("#a4").removeClass("hidden");
+
+});
+
+$("#d5").on("click", function() {
+    $('article').addClass("hidden");
+    $("#a5").removeClass("hidden");
+
+});
+
+$("#d6").on("click", function() {
+    $('article').addClass("hidden");
+    $("#a6").removeClass("hidden");
+
+});
+
+$("#d7").on("click", function() {
+    $('article').addClass("hidden");
+    $("#a7").removeClass("hidden");
+
+});
