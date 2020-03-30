e0f216925597dc83d6c3733f82a69d3ed08b4ae7
Turqay Umudzade <turgayau@code.edu.az>
2020-03-30 06:55:48 +0400

    location.html fully done and responsive

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -4,5 +4,46 @@ $('.custom-selector').focus(function() {
     $(".custom-options").removeClass("hidden");
 }).blur(function() {
     $(this).attr('placeholder', 'Filter by State/Province');
-    $(".custom-options").addClass("hidden");
+    setTimeout(function() { $(".custom-options").addClass("hidden") }, 400);
 })
+
+$('.custom-options > li').click(function(e) {
+    $('.custom-selector').val($(this).text());
+});
+
+$('.custom-selector').on("change", function() {
+    var s = $('.custom-selector').val();
+    paymentUrl = "/Locations/search?searchStr=" + s;
+    $.ajax({
+        url: paymentUrl,
+        type: "get",
+        dataType: "html",
+        beforeSend: function() {},
+        success: function(response) {
+            $(".locations").html(response);
+        },
+        error: function(error) {
+            $.notify("Error not found", "error");
+        },
+        complete: function() {}
+    });
+});
+
+
+$('.custom-selector').on("change paste keyup", function() {
+    var s = $('.custom-selector').val();
+    paymentUrl = "/Locations/selectFill?searchStr=" + s;
+    $.ajax({
+        url: paymentUrl,
+        type: "get",
+        dataType: "html",
+        beforeSend: function() {},
+        success: function(response) {
+            $(".custom-options").html(response);
+        },
+        error: function(error) {
+            $.notify("Error not found", "error");
+        },
+        complete: function() {}
+    });
+});
